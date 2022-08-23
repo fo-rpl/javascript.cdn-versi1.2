856838bb4d5886fa65f739a4915fba62091ccd7c
fo-rpl <toktokgaming1@gmail.com>
2022-08-23 22:32:17 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -181,4 +181,127 @@ document.getElementById('topbar1').innerHTML=`
             </div>
         </div>
 `;
+document.getElementById('navbartop').innerHTML=`
+        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
+            <a href="index.html" class="navbar-brand ml-lg-3">
+                <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>fo-rpl</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+                <div class="navbar-nav m-auto py-0">
+                    <a href="index.html" class="nav-item nav-link">Home</a>
+                    <a href="about.html" class="nav-item nav-link">About</a>
+                    <a href="service.html" class="nav-item nav-link">Service</a>
+                    <a href="price.html" class="nav-item nav-link">Price</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle active" data-toggle="dropdown">Pages</a>
+                        <div class="dropdown-menu rounded-0 m-0">
+                            <a href="blog.html" class="dropdown-item">Blog Grid</a>
+                            <a href="single.html" class="dropdown-item active">Blog Detail</a>
+                        </div>
+                    </div>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
+            </div>
+        </nav>
+`;
+document.getElementById('profilweb').innerHTML=`
+        <div class="container text-center py-5">
+            <h1 class="text-white display-3">Tech7 </h1>
+            <div class="d-inline-flex align-items-center text-white">
+                <p class="m-0"><a class="text-white" href="">Home</a></p>
+                <i class="fa fa-circle px-3"></i>
+                <p class="m-0">Blog</p>
+            </div>
+        </div>
+`;
+document.getElementById('sidebar').innerHTML=`
+                <div class="mb-5">
+                    <div class="bg-secondary" style="padding: 30px;">
+                        <div class="input-group">
+                            <input type="text" class="form-control border-0 p-4" placeholder="Keyword">
+                            <div class="input-group-append">
+                                <span class="input-group-text bg-primary border-primary text-white"><i
+                                        class="fa fa-search"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Search Form End -->
+
+                <!-- Category Start -->
+                <div class="mb-5">
+                    <h3 class="mb-4">Categories</h3>
+                    <div class="bg-secondary" style="padding: 30px;">
+                        <ul class="list-inline m-0">
+                            <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web Design</a>
+                                <span class="badge badge-secondary badge-pill">150</span>
+                            </li>
+                            <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web Development</a>
+                                <span class="badge badge-secondary badge-pill">131</span>
+                            </li>
+                            <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Online Marketing</a>
+                                <span class="badge badge-secondary badge-pill">78</span>
+                            </li>
+                            <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Keyword Research</a>
+                                <span class="badge badge-secondary badge-pill">56</span>
+                            </li>
+                            <li class="py-2 px-3 bg-light d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Email Marketing</a>
+                                <span class="badge badge-secondary badge-pill">98</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Category End -->
+
+                <!-- Recent Post Start -->
+                <div class="mb-5">
+                    <h3 class="mb-4">Recent Post</h3>
+                    <div class="d-flex mb-3">
+                        <img class="img-fluid" src="img/blog-1.jpg" style="width: 80px; height: 80px; object-fit: cover;" alt="">
+                        <a href="" class="d-flex align-items-center bg-secondary text-dark text-decoration-none px-3" style="height: 80px;">
+                            Lorem ipsum dolor sit amet consec adipis elit
+                        </a>
+                    </div>
+                </div>
+                <!-- Recent Post End -->
+
+                <!-- Image Start -->
+
+                <!-- Image End -->
+
+                <!-- Tags Start -->
+                <div class="mb-5">
+                    <h3 class="mb-4">Tag Cloud</h3>
+                    <div class="d-flex flex-wrap m-n1">
+                        <a href="" class="btn btn-secondary m-1">Design</a>
+                        <a href="" class="btn btn-secondary m-1">Development</a>
+                        <a href="" class="btn btn-secondary m-1">Website</a>
+                        <a href="" class="btn btn-secondary m-1">SEO</a>
+                        <a href="" class="btn btn-secondary m-1">Writing</a>
+                        <a href="" class="btn btn-secondary m-1">Consulting</a>
+                    </div>
+                </div>
+                <!-- Tags End -->
+
+                <!-- Image Start -->
+             
+                <!-- Image End -->
+
+                <!-- Plain Text Start -->
+                <div>
+                    <h3 class="mb-4">Plain Text</h3>
+                    <div class="bg-secondary text-center" style="padding: 30px;">
+                        <p>Vero sea et accusam justo dolor accusam lorem consetetur, dolores sit amet sit dolor clita kasd justo, diam accusam no sea ut tempor magna takimata, amet sit et diam dolor ipsum amet diam</p>
+                        <a href="" class="btn btn-primary py-2 px-4">Read More</a>
+                    </div>
+                </div>
+`;
 setTimeout(showfooter,2500);
